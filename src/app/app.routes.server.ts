1a3cc904901670efe55b3b654cbcccb91857a3da
EstebanM159 <estebanm91231@gmail.com>
2026-02-18 23:57:01 -0300

    Change pokemos-page render mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,7 +4,7 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'about', renderMode: RenderMode.Prerender },
   { path: 'contact', renderMode: RenderMode.Prerender },
   { path: 'pricing', renderMode: RenderMode.Prerender },
-  { path: 'pokemons', renderMode: RenderMode.Prerender },
+  { path: 'pokemons', renderMode: RenderMode.Server },
   { path: 'pokemon/:id', renderMode: RenderMode.Server },
   { path: '**', renderMode: RenderMode.Server },
 ];
