598c25e5ae5632269770e66e3868add43b9b1207
EstebanM159 <estebanm91231@gmail.com>
2026-03-01 19:20:45 -0300

    feat: implement pagination for Pokémon pages and update routing

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,7 +4,15 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'about', renderMode: RenderMode.Prerender },
   { path: 'contact', renderMode: RenderMode.Prerender },
   { path: 'pricing', renderMode: RenderMode.Prerender },
-  { path: 'pokemons', renderMode: RenderMode.Server },
+  // { path: 'pokemons', renderMode: RenderMode.Server },
   { path: 'pokemon/:id', renderMode: RenderMode.Server },
+  {
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      console.log('PRERENDER DE PAGE');
+      return [{ page: '1' }];
+    },
+  },
   { path: '**', renderMode: RenderMode.Server },
 ];
--- src/app/pokemons/pages/pokemons-page/pokemons-page.ts
+++ src/app/pokemons/pages/pokemons-page/pokemons-page.ts
@@ -1,45 +1,40 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { PokemonList } from '../../components/pokemon-list/pokemon-list';
 import { PokemonsListSkeleton } from '../../ui/pokemons-list-skeleton/pokemons-list-skeleton';
 import { PokemonService } from '../../services/pokemon-service';
 import { SimplePokemon } from '../../interfaces';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map, tap } from 'rxjs';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemons-page',
-  imports: [PokemonList, PokemonsListSkeleton],
+  imports: [PokemonList, PokemonsListSkeleton, RouterLink],
   templateUrl: './pokemons-page.html',
 })
-export default class PokemonsPage implements OnInit {
+export default class PokemonsPage {
   pokemonService = inject(PokemonService);
   private route = inject(ActivatedRoute);
-  private router = inject(Router);
   title = inject(Title);
   public isLoading = signal(true);
   public pokemons = signal<SimplePokemon[]>([]);
   public currentPage = toSignal<number>(
-    this.route.queryParamMap.pipe(
-      map((params) => params.get('page') ?? '1'),
+    this.route.params.pipe(
+      map((params) => params['page'] ?? '1'),
       map((page) => (isNaN(+page) ? 1 : +page)),
       map((page) => Math.max(1, page)),
     ),
   );
 
-  ngOnInit(): void {
-    console.log(this.currentPage());
-    this.loadPokemons();
-  }
+  public loadOnPage = effect(() => {
+    this.loadPokemons(this.currentPage());
+  });
   loadPokemons(page: number = 0) {
     const pageToLoad = this.currentPage()! + page;
     this.pokemonService
       .loadPage(pageToLoad)
-      .pipe(
-        tap(() => this.router.navigate([], { queryParams: { page: pageToLoad } })),
-        tap(() => this.title.setTitle(`Pokémons SSR - Page ${pageToLoad}`)),
-      )
+      .pipe(tap(() => this.title.setTitle(`Pokémons SSR - Page ${pageToLoad}`)))
       .subscribe((pokemons) => {
         this.pokemons.set(pokemons);
       });
